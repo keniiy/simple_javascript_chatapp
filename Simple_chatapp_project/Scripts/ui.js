fa6ccdf2109309093d02ab2c2b53b4bb3b307ea2
kehinde kehinde peter <kehindekehinde894@gmail.com>
2020-08-03 14:11:01 -0700

    Add files via upload

--- Simple_chatapp_project/Scripts/ui.js
+++ Simple_chatapp_project/Scripts/ui.js
@@ -1,25 +1,22 @@
-//render chat to the dom
-
-//clear the list of chat (whem the room changes)
-
-
 class ChatUI {
     constructor(list){
-        this.list = list;
+      this.list = list;
+    }
+    clear(){
+      this.list.innerHTML = '';
     }
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix: true }
-        )
-        const html = `
+      const when = dateFns.distanceInWordsToNow(
+        data.created_at.toDate(),
+        { addSuffix:true }
+      );
+      const html = `
         <li class="list-group-item">
-         <span class="username">${data.username}</span>
-         <span class="message">${data.message}</span>
-         <div class="time">${when}</div>
+          <span class="username">${data.username}</span>
+          <span class="message">${data.message}</span>
+          <div class="time">${when}</span>
         </li>
-        `;
-
-        this.list.innerHTML += html;
+      `;
+      this.list.innerHTML += html;
     }
-}
+  }
